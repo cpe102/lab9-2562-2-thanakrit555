d9bde964159f84430108cd18d5f7d61acbd69327
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-01-28 17:14:55 +0700

    Add files via upload

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,21 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	ifstream source;
+	ofstream dest("C:\\Users\\LaB_314\\Desktop\\cheerbook_coppy.txt");
+	source.open("C:\\Users\\LaB_314\\Desktop\\cheerbook.txt");
+	string textline;
+	dest<<"--------------------- SOTUS ---------------------"<<"\n";
+	bool havetext;
+	havetext = getline(source,textline);
+	while(havetext){
+		dest << textline<<"\n";
+		havetext = getline(source,textline);
+	}
+	dest<<"---------------------SOTUS ---------------------";
+	source.close();
+	dest.close();
+	return 0;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+int main(){
+	
+	int count =0;
+	float sum =0;
+	float y =0;
+	string textline;
+	ifstream source("C:\\Users\\LaB_314\\Desktop\\score.txt"); 
+	while (getline(source,textline)){
+		cout<<textline<<"\n";
+		sum += atof(textline.c_str());
+		y+=pow(atof(textline.c_str()),2);
+		count ++;
+	}
+	cout << "Number of data = "<<count<<"\n";
+    cout << "Mean = "<<sum/count<<"\n";
+    cout << "Standard deviation = "<<sqrt(y/count-pow(sum/count,2)) <<"\n";
+}
